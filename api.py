64fea43de913a7cd30bde9e5190772aa2d2e2fa5
Tanishq-venom-jain <158131581+Tanishq-venom-jain@users.noreply.github.com>
2025-09-20 22:45:04 +0530

    Update api.py

--- api.py
+++ api.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 CORS(app) 
 
 def get_analysis_from_gemini(article_text: str):
-    api_key = os.getenv("AIzaSyAL8hSIbTugM5rIr8GF-hDDoWcuL9mo9Sg")
+    api_key = os.getenv("GOOGLE_API_KEY")
     if not api_key:
         raise ValueError("Error: GOOGLE_API_KEY environment variable not set.")
     genai.configure(api_key=api_key)
